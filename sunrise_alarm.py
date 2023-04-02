d19e4c2411fc0b1f0c5687a6d65990623e354ea7
gpro1 <gpro1@rogers.com>
2023-04-02 19:01:39 -0400

    adjusted alarm timing

--- sunrise_alarm.py
+++ sunrise_alarm.py
@@ -13,7 +13,7 @@ alarm_min = int(alarm_time[1])
 while(True):
     t = datetime.datetime.today()
     future = datetime.datetime(t.year, t.month, t.day, alarm_hour, alarm_min)
-    future -= datetime.timedelta(minutes=30)
+    future -= datetime.timedelta(minutes=15)
     if t.timestamp() > future.timestamp():
         future += datetime.timedelta(days=1)
     time.sleep((future-t).total_seconds())
